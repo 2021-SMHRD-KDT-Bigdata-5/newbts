11733a742484ca25f17916e82f618b42f5021b00
eun6304 <a01050412736@gmail.com>
2021-08-23 13:52:39 +0900

    이미지 업로드

--- Korean_edu/src/main/webapp/sql.sql
+++ Korean_edu/src/main/webapp/sql.sql
@@ -3,23 +3,10 @@ id varchar2(50) primary key not null,
 password varchar2(50) not null,
 name varchar2(50) not null,
 birth date,
-tel number not null,
+tel tel varchar2(50) not null,
 address varchar2(50) not null
 );
 
-alter table member modify tel varchar2(50);
-
-
-create table score(
-	id varchar2(12) primary key not null,
-	score number default 0
-);
-
-drop table member;
-
-alter table member add score number;
-select * from member;
-delete from member where id='eun';
 
 
 create table board(
@@ -29,7 +16,6 @@ create table board(
 	writedate date
 )
 
-select * from member;
 
 create sequence num_board
 increment by 1 
